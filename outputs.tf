5055136bfcdf8e48362fcffb7f3f4a4bdf946820
Jonathon Brouse <jonathon.brouse@spartasystems.com>
2016-11-23 16:47:36 +0000

    Added CIDR blocks to outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -10,15 +10,23 @@ output "private_route_table_ids" {
   value = ["${aws_route_table.private_route_table.*.id}"]
 }
 
-output "private_subnets" {
+output "private_subnet_ids" {
   value = ["${aws_subnet.private_subnet.*.id}"]
+}
+
+output "private_subnet_cidr_blocks" {
+  value = ["${aws_subnet.private_subnet.*.cidr_block}"]
 }
 
 output "public_route_table_ids" {
   value = ["${aws_route_table.public_route_table.*.id}"]
 }
 
-output "public_subnets" {
+output "public_subnet_cidr_blocks" {
+  value = ["${aws_subnet.public_subnet.*.cidr_block}"]
+}
+
+output "public_subnet_ids" {
   value = ["${aws_subnet.public_subnet.*.id}"]
 }
 
